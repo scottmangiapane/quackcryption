ac8f84e53cd374cdf1a08800f08f583f68b03268
Kevin Dong <kevindong@users.noreply.github.com>
2016-02-27 01:16:05 -0500

    Added encrypt and decrypt functions.

--- QuackCryption.java
+++ QuackCryption.java
@@ -4,8 +4,15 @@
  * translate quack encrypted text.
  *
  * @author Vikram
+ * @author Kevin
  * @version 02/26/2016
  */
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
 public class QuackCryption {
 
     /**
@@ -438,6 +445,43 @@ public class QuackCryption {
         return deQuacked;
 
     }
+    
+    public static String encrypt(String key, String initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("ASCII"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("ASCII"), "AES");
 
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            //System.out.println("encrypted string: "
+            //        + Base64.encodeBase64String(encrypted));
+
+            return Base64.encodeBase64String(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String decrypt(String key, String initVector, String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("ASCII"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("ASCII"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
 }
 
