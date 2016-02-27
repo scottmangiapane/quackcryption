c9e030110588c019d6817ab1eb821365e0b29a19
Scott Mangiapane <scottmangiapane@gmail.com>
2016-02-27 18:53:22 -0500

    Encryption detection
    Added “QuAcKqUaCk “ to beginning of file to determine if it is already
    encrypted.

--- java/co/twoduck/quackcryption/MainActivity.java
+++ java/co/twoduck/quackcryption/MainActivity.java
@@ -3,7 +3,6 @@ package co.twoduck.quackcryption;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +12,6 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean isUnlocked;
     private EditText textBox;
     private Encryptor encryptor;
     private ImageView duckIcon;
@@ -22,16 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        isUnlocked = true;
         textBox = (EditText) findViewById(R.id.text_box);
         encryptor = new Encryptor();
         duckIcon = (ImageView) findViewById(R.id.duck);
         duckIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isUnlocked) {
+                if (String.valueOf(textBox.getText()).startsWith("QuAcKqUaCk ")) {
+                    duckIcon.setImageResource(R.drawable.duck_unlocked);
+                    textBox.setText(encryptor.decrypt("QuackQuackQuack!", "RandomInitVector",
+                            encryptor.quackReader(String.valueOf(
+                                    textBox.getText())
+                                    .substring(11))));
+                    textBox.setFocusableInTouchMode(true);
+                    textBox.setOnClickListener(null);
+                } else {
                     duckIcon.setImageResource(R.drawable.duck_locked);
-                    textBox.setText(encryptor.quackCryptor(encryptor.encrypt("QuackQuackQuack!", "RandomInitVector",
+                    textBox.setText("QuAcKqUaCk " + encryptor.quackCryptor(encryptor.encrypt(
+                            "QuackQuackQuack!", "RandomInitVector",
                             String.valueOf(textBox.getText()))));
                     textBox.setFocusable(false);
                     textBox.setOnClickListener(new View.OnClickListener() {
@@ -44,14 +50,7 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(getBaseContext(), "Copied!", Toast.LENGTH_SHORT).show();
                         }
                     });
-                } else {
-                    duckIcon.setImageResource(R.drawable.duck_unlocked);
-                    textBox.setText(encryptor.decrypt("QuackQuackQuack!", "RandomInitVector",
-                            encryptor.quackReader(String.valueOf(textBox.getText()))));
-                    textBox.setFocusableInTouchMode(true);
-                    textBox.setOnClickListener(null);
                 }
-                isUnlocked = !isUnlocked;
                 MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quack);
                 if (mp != null)
                     mp.start();
