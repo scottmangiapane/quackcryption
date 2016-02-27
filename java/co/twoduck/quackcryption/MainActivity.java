bb98fac907e975267eeb7b98073e99dc8654c02d
Michael Bilstein <Micha3lbilstein@gmail.com>
2016-02-27 02:04:14 -0500

    More stuff for Beam

--- java/co/twoduck/quackcryption/MainActivity.java
+++ java/co/twoduck/quackcryption/MainActivity.java
@@ -1,10 +1,14 @@
 package co.twoduck.quackcryption;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.File;
@@ -28,6 +32,49 @@ public class MainActivity extends AppCompatActivity {
         mNfcAdapter.setBeamPushUrisCallback(mFileUriCallback,this);
     }
 
+    // A File object containing the path to the transferred files
+    private File mParentPath;
+    // Incoming Intent
+    private Intent mIntent;
+    /*
+     * Called from onNewIntent() for a SINGLE_TOP Activity
+     * or onCreate() for a new Activity. For onNewIntent(),
+     * remember to call setIntent() to store the most
+     * current Intent
+     *
+     */
+    private void handleViewIntent() {
+        // Get the Intent action
+        mIntent = getIntent();
+        String action = mIntent.getAction();
+        /*
+         * For ACTION_VIEW, the Activity is being asked to display data.
+         * Get the URI.
+         */
+        if (TextUtils.equals(action, Intent.ACTION_VIEW)) {
+            // Get the URI from the Intent
+            Uri beamUri = mIntent.getData();
+            /*
+             * Test for the type of URI, by getting its scheme value
+             */
+            if (TextUtils.equals(beamUri.getScheme(), "file")) {
+                mParentPath = new File(handleFileUri(beamUri));
+            } else if (TextUtils.equals(
+                    beamUri.getScheme(), "content")) {
+                mParentPath = new File(handleContentUri(beamUri));
+            }
+        }
+    }
+
+    public String handleFileUri(Uri beamUri) {
+        // Get the path part of the URI
+        String fileName = beamUri.getPath();
+        // Create a File object for this filename
+        File copiedFile = new File(fileName);
+        // Get a string containing the file's parent directory
+        return copiedFile.getParent();
+    }
+
     private Uri[] mFileUris = new Uri[10];
     private class FileUriCallback implements NfcAdapter.CreateBeamUrisCallback {
         public FileUriCallback() {
@@ -39,6 +86,45 @@ public class MainActivity extends AppCompatActivity {
         public Uri[] createBeamUris(NfcEvent event) {
             return mFileUris;
         }
+    }
+
+    public String handleContentUri(Uri beamUri) {
+        // Position of the filename in the query Cursor
+        int filenameIndex;
+        // File object for the filename
+        File copiedFile;
+        // The filename stored in MediaStore
+        String fileName;
+        // Test the authority of the URI
+        if (!TextUtils.equals(beamUri.getAuthority(), MediaStore.AUTHORITY)) {
+            /*
+             * Handle content URIs for other content providers
+             */
+            // For a MediaStore content URI
+        } else {
+            // Get the column that contains the file name
+            String[] projection = { MediaStore.MediaColumns.DATA };
+            Cursor pathCursor =
+                    getContentResolver().query(beamUri, projection,
+                            null, null, null);
+            // Check for a valid cursor
+            if (pathCursor != null &&
+                    pathCursor.moveToFirst()) {
+                // Get the column index in the Cursor
+                filenameIndex = pathCursor.getColumnIndex(
+                        MediaStore.MediaColumns.DATA);
+                // Get the full file name including path
+                fileName = pathCursor.getString(filenameIndex);
+                // Create a File object for the filename
+                copiedFile = new File(fileName);
+                // Return the parent directory of the file
+                return copiedFile.getParent();
+            } else {
+                // The query didn't work; return null
+                return null;
+            }
+        }
+        return null;
     }
 
     private void updateText() {
