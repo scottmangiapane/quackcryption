3e16e1a69451c2afada3a65de50584673c631beb
Scott Mangiapane <scottmangiapane@gmail.com>
2016-02-27 20:06:16 -0500

    NFC Receive
    Added the receive activity for NFC communication.

--- java/co/twoduck/quackcryption/MainActivity.java
+++ java/co/twoduck/quackcryption/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -26,34 +28,39 @@ public class MainActivity extends AppCompatActivity {
         duckIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (String.valueOf(textBox.getText()).startsWith("QuAcKqUaCk ")) {
-                    duckIcon.setImageResource(R.drawable.duck_unlocked);
-                    textBox.setText(Encryptor.decrypt(preferences.getKey(),
-                            preferences.getInitVector(), Encryptor.quackReader(
-                                    String.valueOf(textBox.getText()).substring(11))));
-                    textBox.setFocusableInTouchMode(true);
-                    textBox.setOnClickListener(null);
-                } else {
-                    duckIcon.setImageResource(R.drawable.duck_locked);
-                    textBox.setText("QuAcKqUaCk " + Encryptor.quackCryptor(Encryptor.encrypt(
-                            preferences.getKey(), preferences.getInitVector(),
-                            String.valueOf(textBox.getText()))));
-                    textBox.setFocusable(false);
-                    textBox.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ClipboardManager clipboard = (ClipboardManager)
-                                    getSystemService(Context.CLIPBOARD_SERVICE);
-                            ClipData clip = ClipData.newPlainText("simple text", "Hello, World!");
-                            clipboard.setPrimaryClip(clip);
-                            Toast.makeText(getBaseContext(), "Copied!", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quack);
-                if (mp != null)
-                    mp.start();
+                refresh();
             }
         });
+    }
+
+    private void refresh() {
+        if (String.valueOf(textBox.getText()).startsWith("QuAcKqUaCk ")) {
+            duckIcon.setImageResource(R.drawable.duck_unlocked);
+            textBox.setText(Encryptor.decrypt(preferences.getKey(),
+                    preferences.getInitVector(), Encryptor.quackReader(
+                            String.valueOf(textBox.getText()).substring(11))));
+            textBox.setFocusableInTouchMode(true);
+            textBox.setOnClickListener(null);
+        } else {
+            duckIcon.setImageResource(R.drawable.duck_locked);
+            textBox.setText("QuAcKqUaCk " + Encryptor.quackCryptor(Encryptor.encrypt(
+                    preferences.getKey(), preferences.getInitVector(),
+                    String.valueOf(textBox.getText()))));
+            textBox.setFocusable(false);
+            textBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ClipboardManager clipboard = (ClipboardManager)
+                            getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("ENCRYPTED STRING",
+                            String.valueOf(textBox.getText()));
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(getBaseContext(), "Copied!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.quack);
+        if (mp != null)
+            mp.start();
     }
 }
---
+++ java/co/twoduck/quackcryption/ReceiveActivity.java
@@ -0,0 +1,12 @@
+package co.twoduck.quackcryption;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ReceiveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
